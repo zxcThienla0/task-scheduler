bee167c1550a1fb7b693c9405e032b0c815ab766
zxcThienla0 <semasema6865@gmail.com>
2025-10-31 11:32:06 +0300

    fix enum shiftType

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config({ override: false })
 const express = require('express');
 const cors = require('cors');
 const userRouter = require('./routes/user-router')
